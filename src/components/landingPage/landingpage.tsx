1f96c1e708bcbd3834af3aa02341f285f30a64af
abd <abedoocool@gmail.com>
2023-10-08 20:31:43 +0300

    deployed website

--- src/components/landingPage/landingpage.tsx
+++ src/components/landingPage/landingpage.tsx
@@ -2,48 +2,39 @@ import React, { useState } from "react";
 import './landing.css';
 import Logo from "../../assets/images/logo.svg"
 import { Button, ChakraProvider, extendTheme, useDisclosure } from "@chakra-ui/react";
-import {
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-  } from '@chakra-ui/react'
-import TheModal from "../modal/modal";
 import CustomModal from "../modal/modal";
 
-interface LandingProps {}
+interface LandingProps { }
 
 const theme = extendTheme({
   colors: {
     custom: {
       500: "#FF8C1E",
     },
-    disapled:{
-      500:"#B3B3B3  "
+    disapled: {
+      500: "#B3B3B3  "
     }
   },
 });
 
 const Landing: React.FC<LandingProps> = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [open,setOpen]=useState<boolean>(false)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [open, setOpen] = useState<boolean>(false)
 
-    const handleEditUser = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setOpen(!open)
-    };
+  const handleEditUser = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setOpen(!open)
+  };
   return (
     <ChakraProvider theme={theme}>
       <div className="bg">
         <div className="center">
-          <img src={Logo} alt="Logo" /> 
-         
-                    <CustomModal />
+          <img src={Logo} alt="Logo" />
+
+          <CustomModal />
 
         </div>
       </div>
-     
+
     </ChakraProvider>
   );
 }
